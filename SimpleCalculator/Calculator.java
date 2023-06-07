4a87099827cc4e76c97ab210caec26559c2d834f
Sunit <kumarsunit017@gmail.com>
2023-06-07 15:54:21 +0530

    This is First One

---
+++ SimpleCalculator/Calculator.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+public class Calculator{
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+        System.out.println("Enter First Number");
+        int a=sc.nextInt();
+        System.out.println("Enter the Second Number");
+        int b=sc.nextInt();
+        System.out.println("Choose the Operator + - * /");
+        String operator=sc.nextLine();
+        int result;
+        switch(operator){
+            case"+":
+            result =a+b;
+            System.out.println("Addition of Number is:"+result);
+            break;
+            case"-":
+            result=a-b;
+            System.out.println("Substraction of Number is "+result);
+            break;
+            case"*":
+            result=a*b;
+            System.out.println("Multiplication is"+result);
+            break;
+            case"/":
+            result=a/b;
+            System.out.println("Division is"+result);
+            break;
+            default:
+            System.out.println("Invalid Number");
+                }
+
+    }
+}
